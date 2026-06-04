99e76156a92ba571276f66cbc823838fc4d19fe1
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-04 13:44:05 +0530

    Aborted the plan of Deploying FastAPI

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,31 +1,11 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = None
-
-
-def get_model():
-    global model
-    if model is None:
-        model = SentenceTransformer(
-            "all-MiniLM-L6-v2"
-        )
-
-    return model
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def create_embeddings(chunks):
     try:
-
-        model = get_model()
-
-        embeddings = (
-            model.encode(
-                chunks,
-                convert_to_numpy=True
-            )
-            .astype(np.float32)
-        )
-
+        embeddings = model.encode(chunks,convert_to_numpy=True).astype(np.float32)
         return embeddings
 
     except Exception as e:
--- rag/retriever.py
+++ rag/retriever.py
@@ -1,29 +1,17 @@
-from rag.embeddings import get_model
+from rag.embeddings import model
 import numpy as np
 
-
 def retrieve(chunks, index, query, top_k=3):
-
-    model = get_model()
-
     query_embedding = model.encode([query])
     query_embedding = np.array(query_embedding).astype("float32")
-
-    distances, indices = index.search(
-        query_embedding,
-        top_k
-    )
+    distances, indices = index.search(query_embedding,top_k)
 
     retrieved_chunks = []
-
     for idx in indices[0]:
-
         if idx != -1:
-
             retrieved_chunks.append(
                 {
                     "text": chunks[idx]
                 }
             )
-
     return retrieved_chunks

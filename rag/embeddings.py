2d6f298bbf11a5048f5226735cbc8a57e56778b7
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-04 13:12:48 +0530

    Optimize Model Loading for deployment

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,12 +1,34 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    return model
 
 def create_embeddings(chunks):
     try:
-        embeddings = model.encode(chunks, convert_to_numpy=True).astype(np.float32)
+
+        model = get_model()
+
+        embeddings = (
+            model.encode(
+                chunks,
+                convert_to_numpy=True
+            )
+            .astype(np.float32)
+        )
+
         return embeddings
 
     except Exception as e:
-        raise Exception(f"Embedding Error: {str(e)}")
+        raise Exception(
+            f"Embedding Error: {str(e)}"
+        )

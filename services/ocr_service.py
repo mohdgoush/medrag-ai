6653e968482b454176bcb22f93e839a53c39f740
mohdgoush <mohammadgoush00000@gmail.com>
2026-06-04 13:03:19 +0530

    Made some changes for deployment

--- services/ocr_service.py
+++ services/ocr_service.py
@@ -1,8 +1,12 @@
-import pytesseract
 from PIL import Image
 import re
+import os
+import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = (r"C:\Program Files\Tesseract-OCR\tesseract.exe")
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
 
 def extract_text_from_image(image_path):
     try:
